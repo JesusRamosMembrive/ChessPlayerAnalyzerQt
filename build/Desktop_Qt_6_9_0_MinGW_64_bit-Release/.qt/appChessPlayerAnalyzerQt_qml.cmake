d0777321f393176203442eb3a107392a25db4c63
JesusRamosMembrive <ramos.membrive@gmail.com>
2025-07-03 15:19:50 +0200

    improving visualization

---
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/.qt/appChessPlayerAnalyzerQt_qml.cmake
@@ -0,0 +1,17 @@
+
+set(target "appChessPlayerAnalyzerQt")
+set(working_dir "H:/Git_Overture/ChessPlayerAnalyzerQt")
+set(src_and_dest_list
+    "H:/Git_Overture/ChessPlayerAnalyzerQt/Main.qml"
+    "H:/Git_Overture/ChessPlayerAnalyzerQt/build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/ChessPlayerAnalyzerQt/Main.qml"
+    "H:/Git_Overture/ChessPlayerAnalyzerQt/FeatureCard.qml"
+    "H:/Git_Overture/ChessPlayerAnalyzerQt/build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/ChessPlayerAnalyzerQt/FeatureCard.qml"
+    "H:/Git_Overture/ChessPlayerAnalyzerQt/MetricCard.qml"
+    "H:/Git_Overture/ChessPlayerAnalyzerQt/build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/ChessPlayerAnalyzerQt/MetricCard.qml"
+    "H:/Git_Overture/ChessPlayerAnalyzerQt/ResultsPage.qml"
+    "H:/Git_Overture/ChessPlayerAnalyzerQt/build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/ChessPlayerAnalyzerQt/ResultsPage.qml"
+    "H:/Git_Overture/ChessPlayerAnalyzerQt/Card.qml"
+    "H:/Git_Overture/ChessPlayerAnalyzerQt/build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/ChessPlayerAnalyzerQt/Card.qml"
+
+)
+set(timestamp_file "H:/Git_Overture/ChessPlayerAnalyzerQt/build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/.qt/appChessPlayerAnalyzerQt_qml.txt")
